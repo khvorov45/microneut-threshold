11e2d19dadf8c0b0c04636e9d9da9257cfdbcbda
Arseniy Khvorov <khvorov45@gmail.com>
2020-05-14 12:39:56 +1000

    resamples of 2-stage test

--- resample/resample.R
+++ resample/resample.R
@@ -13,6 +13,30 @@ resample_dir <- here::here("resample")
 
 source(file.path(data_dir, "read_data.R"))
 
+test_elisa_micro <- function(data, index, threshold_elisa,
+                             logthreshold_titre,
+                             elisaname = "logtitre2",
+                             titrename = "logtitre") {
+  logtitre <- rlang::sym(titrename)
+  logtitre2 <- rlang::sym(elisaname)
+  data %>%
+    filter(!is.na(inf), !is.na(!!logtitre), !is.na(!!logtitre2)) %>%
+    mutate(
+      micro_only = if_else(!!logtitre < logthreshold_titre, 0L, 1L),
+      elisa_micro = case_when(
+        !!logtitre2 > threshold_elisa ~ 1L,
+        !!logtitre > logthreshold_titre ~ 1L,
+        TRUE ~ 0L
+      ),
+    ) %>%
+    group_by(inf) %>%
+    summarise(
+      prop_pos1 = sum(micro_only) / n(),
+      prop_pos2 = sum(elisa_micro) / n(), index = index,
+      logthreshold_titre = logthreshold_titre, threshold_elisa = threshold_elisa
+    )
+}
+
 test_micro_only <- function(data, index, logthreshold,
                             titrename = "logtitre1") {
   logtitre <- rlang::sym(titrename)
@@ -23,7 +47,7 @@ test_micro_only <- function(data, index, logthreshold,
     ) %>%
     group_by(inf) %>%
     summarise(
-      prop_pos = sum(micro_only) / n(), index = index, type = "micro_only",
+      prop_pos = sum(micro_only) / n(), index = index,
       logthreshold = logthreshold
     )
 }
@@ -37,6 +61,16 @@ calc_sens_spec <- function(results) {
     rename(test_char = prop_pos, char = inf)
 }
 
+calc_sens_spec2 <- function(results) {
+  results %>%
+    mutate(
+      prop_pos1 = if_else(inf == 1L, prop_pos1, 1 - prop_pos1),
+      prop_pos2 = if_else(inf == 1L, prop_pos2, 1 - prop_pos2),
+      inf = if_else(inf == 1L, "Sensitivity", "Specificity"),
+    ) %>%
+    rename(test_char1 = prop_pos1, test_char2 = prop_pos2, char = inf)
+}
+
 resample_and_test <- function(data, n_resamples, micro_thresholds,
                               titrename = "logtitre1") {
   boots <- bootstraps(data)$splits
@@ -47,6 +81,30 @@ resample_and_test <- function(data, n_resamples, micro_thresholds,
     }
   )
   calc_sens_spec(res)
+}
+
+resample_and_test_2 <- function(data, n_resamples, micro_thresholds,
+                                elisa_thresholds,
+                                titrename = "logtitre1",
+                                elisaname = "logtitre2") {
+  boots <- bootstraps(data)$splits
+  res_elisa_micro <- future_map_dfr(
+    elisa_thresholds,
+    function(elisa_threshold) {
+      map_dfr(
+        micro_thresholds,
+        function(micro_threshold) {
+          imap_dfr(
+            boots, ~ test_elisa_micro(
+              analysis(.x), .y, elisa_threshold, micro_threshold, elisaname,
+              titrename
+            )
+          )
+        }
+      )
+    }
+  )
+  calc_sens_spec2(res_elisa_micro)
 }
 
 save_resample <- function(data, name) {
@@ -69,4 +127,26 @@ res <- list(
   )
 )
 
-iwalk(res, save_resample)
+res2 <- list(
+  sim2 = resample_and_test_2(
+    sim_data, 25,
+    seq(log(20), log(80), length.out = 10),
+    seq(log(20), log(80), length.out = 10)
+  ),
+  "suellen-iga" = resample_and_test_2(
+    suellen_data, 25,
+    seq(log(20), log(80), length.out = 10),
+    seq(2.5, 5, length.out = 5),
+    "logtitre", "iga"
+  ),
+  "suellen-igg" = resample_and_test_2(
+    suellen_data, 25,
+    seq(log(20), log(80), length.out = 10),
+    seq(0, 2.5, length.out = 5),
+    "logtitre", "igg"
+  )
+)
+
+all_res <- c(res, res2)
+
+iwalk(all_res, save_resample)
--- resample/summ.R
+++ resample/summ.R
@@ -15,11 +15,29 @@ read_resample <- function(name) {
 
 summ <- function(result) {
   result %>%
-    group_by(char, type, logthreshold) %>%
+    group_by(char, logthreshold) %>%
     summarise(
       test_char_low = quantile(test_char, 0.025),
       test_char_high = quantile(test_char, 0.975),
       test_char = mean(test_char),
+    ) %>%
+    ungroup()
+}
+
+summ2 <- function(result) {
+  result %>%
+    mutate(diff = test_char1 - test_char2) %>%
+    group_by(char, logthreshold_titre, threshold_elisa) %>%
+    summarise(
+      test_char1_low = quantile(test_char1, 0.025),
+      test_char1_high = quantile(test_char1, 0.975),
+      test_char1 = mean(test_char1),
+      test_char2_low = quantile(test_char2, 0.025),
+      test_char2_high = quantile(test_char2, 0.975),
+      test_char2 = mean(test_char2),
+      diff_low = quantile(diff, 0.025),
+      diff_high = quantile(diff, 0.975),
+      diff = mean(diff)
     ) %>%
     ungroup()
 }
@@ -33,5 +51,18 @@ save_summ <- function(summ, name) {
 # Script ======================================================================
 
 resmpls <- map(c("sim" = "sim", "suellen" = "suellen"), read_resample)
+resmpls2 <- map(
+  c(
+    "sim2" = "sim2",
+    "suellen-iga" = "suellen-iga",
+    "suellen-igg" = "suellen-igg"
+  ),
+  read_resample
+)
+
 summs <- map(resmpls, summ)
-iwalk(summs, save_summ)
+summs2 <- map(resmpls2, summ2)
+
+all_summ <- c(summs, summs2)
+
+iwalk(all_summ, save_summ)
